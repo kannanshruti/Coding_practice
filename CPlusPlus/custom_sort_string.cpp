d28c6818633500380fe441ed06df45fdb78c062f
kshruti <kshruti@bu.edu>
2018-08-07 08:56:16 -0400

    custom_sort_string

--- CPlusPlus/custom_sort_string.cpp
+++ CPlusPlus/custom_sort_string.cpp
@@ -10,15 +10,23 @@ public:
 };
 
 string Solution::customSortString(string S, string T) {
+    /*
+    Sorts string T based on the order in S
+    @param "S": String based on which sorting is to be performed
+                Cannot have repetitive characters
+           "T": String which is to be sorted
+    @return "T": Sorted string
+
+    */
     vector<int>order(26,100);
     for(int i = 0; i<S.size(); i++) {
-        order[S[i] - 'a'] = i;
+        order[S[i] - 'a'] = i; // Finding the characters and their order in S
     }
 
     char c;
     for (int i = 0; i < T.length(); i++) {
         for (int j = 0; j < T.length() - 1; j++) {
-            if (order[T[j]-'a'] > order[T[j+1]-'a']) {
+            if (order[T[j]-'a'] > order[T[j+1]-'a']) { // Checking if order is to be changed
                 c = T[j];
                 T[j] = T[j+1];
                 T[j+1] = c;                    
