abddd2a23a3cee25be2ad419ca3cae8fb3f93e14
kshruti <kshruti@bu.edu>
2018-09-28 08:17:15 -0400

    Added operations to linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -21,6 +21,10 @@ public:
 	void deletePosition();
 	void updatePosition();
 	void searchElement();
+	void midList();
+	void deleteMid();
+	void reverse();
+	void addOne();
 };
 
 list::list() {
@@ -213,6 +217,79 @@ void list::searchElement() {
 	}
 }
 
+void list::midList() {
+	node *temp1, *temp2;
+	temp1 = head;
+	temp2 = head;
+	if (head != NULL) {
+		while (temp2 != NULL && temp2->next != NULL) {
+			temp1 = temp1->next;
+			temp2 = temp2->next->next;
+		}
+		cout << "Element at the middle: " << temp1->data << "\n";
+	}
+}
+
+void list::deleteMid() {
+	node *temp1, *temp2, *prev;
+	temp1 = head;
+	temp2 = head;
+	if (head != NULL) {
+		while(temp2 != NULL && temp2->next != NULL) {
+			prev = temp1;
+			temp1 = temp1->next;
+			temp2 = temp2->next->next;
+		}
+		prev->next = temp1->next;
+	}
+}
+
+void list::reverse() {
+	node *prev = NULL, *curr = head, *next = NULL; 
+
+	if (head != NULL) {
+		while(curr != NULL) {
+			next = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = next;
+		}
+		head = prev;
+	}
+}
+
+void list::addOne() {
+	node *temp;
+	int carry = 1, sum = 0;
+	reverse();
+	temp = head;
+	if (head != NULL) {
+		while (temp != NULL) {
+			sum = carry + temp->data;
+			if (sum > 9) {
+				carry = 1;
+				temp->data = 0;
+			}
+			else {
+				carry = 0;
+				temp->data = sum;
+			}
+			temp = temp->next;
+		}
+		reverse();
+	}
+}
+
+void reverse_group() {
+	node *temp;
+	int group;
+
+	cout << "Enter group number: ";
+	cin >> group;
+
+	
+}
+
 int main() {
 	list list1;
 	int choice = 0;
@@ -225,7 +302,12 @@ int main() {
 		cout << "5. Delete @position\n";
 		cout << "6. Update @position\n";
 		cout << "7. Search @value\n";
-		cout << "10. Exit\n";
+		cout << "8. Print mid value\n";
+		cout << "9. Delete mid value\n";
+		cout << "10. Reverse list\n";
+		cout << "11. Add 1 to number repr. by list\n";
+		cout << "12. Reverse in groups of k\n";
+		cout << "20. Exit\n";
 		cout << "Option:";
 
 		cin >> choice;
@@ -245,9 +327,18 @@ int main() {
 				list1.updatePosition(); break;
 			case 7:
 				list1.searchElement(); break;
+			case 8:
+				list1.midList(); break;
+			case 9:
+				list1.deleteMid(); break;
 			case 10:
-				exit(1);
-				break;	
+				list1.reverse(); break;
+			case 11:
+				list1.addOne(); break;
+			case 12:
+				list1.reverse_group(); break;
+			case 20:
+				exit(1); break;	
 		}
 	}
 }
