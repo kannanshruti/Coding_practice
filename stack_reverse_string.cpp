dfaeeaea94a4e987a51ea2a6580e7b71bf46f02f
kshruti <kshruti@bu.edu>
2018-09-30 11:43:24 -0400

    Stack based string reversal using only stack operations

--- stack_reverse_string.cpp
+++ stack_reverse_string.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
+
+stack <char> s;
 
 struct Stack {
 	int top;
@@ -30,7 +33,6 @@ bool Stack_opn::isEmpty() {
 char Stack_opn::pop() {
 	if (isEmpty())
 		return -1;
-	// cout << "Popping:" << stack->arr[stack->top] << "\n";
 	char ch = stack->arr[stack->top];
 	--stack->top;
 	return ch;
@@ -38,30 +40,65 @@ char Stack_opn::pop() {
 
 void Stack_opn::push(char c) {
 	stack->arr[++stack->top] = c;
-	cout << stack->top << " ";
-	// cout << "Pushing:" << stack->arr[stack->top] << "\n";
 }
 
 string reverse(Stack_opn k) {
 	string s;
 	while(!k.isEmpty()) {
 		s += k.pop();
-		cout << s << " ";
 	}
 	return s;
 }
 
+char insertAtBottom(char x) {
+	// 1. If stack empty, i.e. latest element, push to stack
+	// 2. else pop top element, call same at latest element, push top element
+	// 1 inserted, return to reverse, get 2, remove 1, insert 2, push 1, now stack: 1,2. return to reverse
+	// now x is 3, stack not empty, so remove 1, call, remove 2, call, stack empty, push 3
+	// return to 2, push 2, return to 1, push 1. now stack: 1,2,3 ETC.
+	
+	if (s.empty()) { // 1.
+		s.push(x);
+	}
+	else {
+		char a = s.top(); // 2.
+		s.pop();
+		insertAtBottom(x);
+		s.push(a);
+	}
+}
+
+char reverse2() {
+	// If stack size > 0, pop char, reverse
+	// Last one, call isnert at bottom
+	// if orig 4,3,2,1, first go into 4, go into 3, go into 2, go into 1
+	if (!s.empty()) {
+		char x = s.top(); // first 4, next 3, ..
+		s.pop();
+		reverse2();
+		insertAtBottom(x);
+	}
+}
+
+
 int main() {
-	string str = "qwerty", str_rev = "";
+	string str = "1234", str_rev = "", stack_ip = "";
 	Stack_opn s1(str.length());
-	cout << s1.stack->capacity << "\n";
-	cout << s1.stack->top << "\n";
 	
 	for (int i = 0; i < str.length(); i++) {
-		cout << str[i] << " ";
 		s1.push(str[i]);
-		cout << "\n";
+		s.push(str[str.length()-i-1]);
 	}
+
 	str_rev = reverse(s1);
 	cout << "Reverse:" << str_rev << "\n";
+
+	cout << "Reverse2:";
+	reverse2();
+	while (!s.empty()) {
+		char c = s.top();
+		s.pop();
+		cout << c;
+	}
+	cout << "\n";
 }
